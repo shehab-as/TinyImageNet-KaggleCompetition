72979156cbab812190996fdf81eef32023523cad
Shehab Abdel-Salam <shehab_m@aucegypt.edu>
2017-04-08 22:45:43 +0200

    cannot do validation split on model.fit_generator(). Added a validation_generator.

--- train_with_Xception.py
+++ train_with_Xception.py
@@ -10,18 +10,34 @@ import time
 
 now = time.strftime("%c")
 nb_of_classes = 200
-# Fixing Seed...
+batch_size = 64
+train_dir_path = 'data/train'
+epochs = 1000
+test_dir_path = 'test_images'   # test path not used yet...
+
+# Fixed Seed and callbacks...
 np.random.seed(seed=204)
 tensorboard_callback = TensorBoard(log_dir="./logs/training_" + now, histogram_freq=0, write_graph=True, write_images=False)
 checkpoint_callback = ModelCheckpoint(filepath="./best_acc_weights.hdf5", verbose=1, save_best_only=True, monitor="val_acc")
+callbacks = [tensorboard_callback, checkpoint_callback]
 
-### Loading Data
-X_train = None
-Y_train = None
-X_validate = None
-Y_validate = None
+#   Using Xception pre-trained Network...
+pre_trained_model = Xception(weights='imagenet', include_top=False)
+x = pre_trained_model.output
+#   Adding Layers on top of it...
+x = Dense(1024, activation='relu')(x)
+predictions = Dense(nb_of_classes, activation='softmax')(x)
 
-train_datagen = ImageDataGenerator(
+#   Actual Model...
+model = Model(input=pre_trained_model.input, output=predictions)
+adam = Adam(lr=0.0001)
+model.compile(optimizer=adam, loss='categorical_crossentropy')
+
+
+
+                                # Loading Data...
+# Data Generation...
+train_data_generator = ImageDataGenerator(
     featurewise_center=True,  # set input mean to 0 over the dataset
     samplewise_center=False,  # don't set each sample mean to 0
     featurewise_std_normalization=True,  # divide all inputs by std of the dataset
@@ -34,31 +50,17 @@ train_datagen = ImageDataGenerator(
     width_shift_range=0.1,
     height_shift_range=0.1
 )
-
-validate_datagen = ImageDataGenerator(
+validate_data_generator = ImageDataGenerator(
     featurewise_center=True,  # test images should have input mean set to 0 over the images.
     featurewise_std_normalization=True,  # test images should have all divided by std of the images.
     zca_whitening=False
 )
 
-# Model Fitting with Datagen #
-train_datagen.fit(X_train)
-validate_datagen.fit(X_validate)
+train_data_generator = train_data_generator.flow_from_directory(train_dir_path, batch_size=batch_size,
+                                                  shuffle=True, class_mode='binary')
+# validate_data_generator = validate_data_generator.flow_from_directory()
 
-    ###     Using Xception pre-trained Network      ###
-pre_trained_model = Xception(weights='imagenet', include_top=False)
-x = pre_trained_model.output
-    ###     Adding Layers on top of it      ###
-x = Dense(1024, activation='relu')(x)
-predictions = Dense(nb_of_classes, activation='softmax')(x)
-
-    ###     Actual Model   ###
-model = Model(input=pre_trained_model.input, output=predictions)
-adam = Adam(lr=0.0001)
-model.compile(optimizer=adam, loss='categorical_crossentropy')
-
-
-
-model.fit_generator(train_datagen.flow_from_directory(), nb_epoch=1000,
-                    validation_data= validate_datagen.flow_from_directory(),
-                    verbose=1, callbacks=[tensorboard_callback, checkpoint_callback])
+model.fit_generator(train_data_generator,
+                    nb_epoch=epochs,
+                    validation_data=validate_data_generator,
+                    verbose=1, callbacks=callbacks)
