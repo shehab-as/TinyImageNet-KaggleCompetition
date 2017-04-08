723d68267fedc13ba95ce466e9e9216ca589f06a
Shehab Abdel-Salam <shehab_m@aucegypt.edu>
2017-04-09 01:24:22 +0200

    cannot do validation split on model.fit_generator(). Added a validation_generator.

--- train_with_Xception.py
+++ train_with_Xception.py
@@ -3,40 +3,49 @@ from keras.preprocessing import image as image_utils
 from keras.preprocessing.image import ImageDataGenerator
 from keras.applications import Xception
 from keras.models import Model
-from keras.layers import Dense
+from keras.layers import Input, Dense, GlobalAveragePooling2D
 from keras.optimizers import Adam
 from keras.callbacks import TensorBoard, ModelCheckpoint
 import time
 
 now = time.strftime("%c")
 nb_of_classes = 200
+img_height = 299
+img_width = 299
 batch_size = 64
 train_dir_path = 'data/train'
-epochs = 1000
-test_dir_path = 'test_images'   # test path not used yet...
+nb_of_epochs = 500
+test_dir_path = 'test_images'  # test path not used yet...
 
-# Fixed Seed and callbacks...
+#   Fixed Seed and callbacks...
 np.random.seed(seed=204)
-tensorboard_callback = TensorBoard(log_dir="./logs/training_" + now, histogram_freq=0, write_graph=True, write_images=False)
-checkpoint_callback = ModelCheckpoint(filepath="./best_acc_weights.hdf5", verbose=1, save_best_only=True, monitor="val_acc")
+tensorboard_callback = TensorBoard(log_dir="./logs/training_" + now, histogram_freq=0, write_graph=True,
+                                   write_images=False)
+checkpoint_callback = ModelCheckpoint(filepath="./top_acc_weights.hdf5", verbose=1, save_best_only=True,
+                                      monitor="val_acc")
 callbacks = [tensorboard_callback, checkpoint_callback]
+input_tensor = Input(shape=(img_width, img_height, 3))
 
 #   Using Xception pre-trained Network...
-pre_trained_model = Xception(weights='imagenet', include_top=False)
+pre_trained_model = Xception(weights='imagenet', input_tensor=input_tensor, include_top=False)
 x = pre_trained_model.output
-#   Adding Layers on top of it...
+x = GlobalAveragePooling2D()(x)
+
+#   freeze CNN layers...
+for layer in pre_trained_model.layers:
+    layer.trainable = False
+
+#   Adding Fully Connected Layer on top of it...
 x = Dense(1024, activation='relu')(x)
 predictions = Dense(nb_of_classes, activation='softmax')(x)
 
-#   Actual Model...
+#   Building Model...
 model = Model(input=pre_trained_model.input, output=predictions)
 adam = Adam(lr=0.0001)
 model.compile(optimizer=adam, loss='categorical_crossentropy')
 
-
-
-                                # Loading Data...
-# Data Generation...
+#   Loading Data...
+#   Data Generation...
 train_data_generator = ImageDataGenerator(
     featurewise_center=True,  # set input mean to 0 over the dataset
     samplewise_center=False,  # don't set each sample mean to 0
@@ -56,11 +65,19 @@ validate_data_generator = ImageDataGenerator(
     zca_whitening=False
 )
 
-train_data_generator = train_data_generator.flow_from_directory(train_dir_path, batch_size=batch_size,
-                                                  shuffle=True, class_mode='binary')
-# validate_data_generator = validate_data_generator.flow_from_directory()
+#   Loading Dataset TinyImageNet
+train_data_generator = train_data_generator.flow_from_directory(train_dir_path, target_size=(img_width, img_height),
+                                                                batch_size=batch_size,
+                                                                shuffle=True,
+                                                                class_mode='binary')
+# validate_data_generator = validate_data_generator.flow_from_directory(..., target_size=(img_width, img_height),
+#                                                                       batch_size=batch_size,
+#                                                                       shuffle=False,
+#                                                                       class_mode='binary')
 
+print("Dataset loaded!")
+print("Now Training...")
 model.fit_generator(train_data_generator,
-                    nb_epoch=epochs,
+                    epochs=nb_of_epochs,
                     validation_data=validate_data_generator,
                     verbose=1, callbacks=callbacks)
